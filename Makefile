cbd62d1cd6652b8fdf6e3ca7cbc5a110abba8e73
metastable-void <git@yuka.h09.eu>
2021-07-14 07:26:59 +0000

    updates

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
-deconoha:
-	cc -o deconoha deconoha.c
+all: bin/deconoha bin/deconoha-png bin/deconoha-ogg
+
+bin/deconoha-png:
+	mkdir -p bin
+	cc -o bin/deconoha-png deconoha-png.c
+
+bin/deconoha-ogg:
+	mkdir -p bin
+	cc -o bin/deconoha-ogg deconoha-ogg.c
+
+bin/deconoha:
+	mkdir -p bin
+	cc -o bin/deconoha deconoha.c
 
 clean:
-	rm -f deconoha
+	rm -rf bin
--- deconoha-png.c
+++ deconoha-png.c
@@ -15,12 +15,13 @@ int main(int argc, char* argv[]) {
   }
   char buffer[2];
   size_t pos = 0;
-  fprintf(stderr, "hash (hex): ");
   char mask0 = (char) 0;
-  scanf("%hhx", &mask0);
   while (!feof(fp)) {
     size_t read = fread(buffer, 1, 1, fp);
     if (read < 1) continue;
+    if (pos == 0) {
+      mask0 = buffer[0] ^ ((char) 0x89);
+    }
     if (pos & 1) {
       buffer[0] ^= (char) (255 & pos);
     } else {
